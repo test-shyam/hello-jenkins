5858d44059534abc26cd85f2bd3d06e92c595e12
Sudesh Athippatta <testmachineforurolime@gmail.com>
2017-09-18 18:34:34 +0000

    information changed

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php phpinfo(); ?>
+<?php
 
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
